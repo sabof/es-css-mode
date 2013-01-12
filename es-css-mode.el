b90cabb632c0d300310c3127cb1eb004e8321b19
sabof <sabof@example.com>
2013-01-12 11:25:35 +0000

    licence header

--- es-css-mode.el
+++ es-css-mode.el
@@ -1,10 +1,40 @@
+;;; es-css-mode.el --- CSS major mode featuring correct indentation for css and scss files
+;;; Version: 0.1
+;;; Author: sabof
+;;; URL: https://github.com/sabof/es-css-mode
+
+;;; Commentary:
+
+;; The project is hosted at https://github.com/sabof/es-css-mode
+;; The latest version, and all the relevant information can be found there.
+
+;;; License:
+
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program ; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Code:
+
 (require 'css-mode)
 (require 'es-lib)
-(require 'flymake)
 
 ;; For es-sass-mode
 (defvar es-css-comment-line-p-function 'es-css-comment-line-p
-  "Should return 1 if at the beginning of a comment, t if inside")
+  "Should return 1 if at the beginning of a comment, t if inside.")
 
 (defun es-css-comment-line-p ()
   (interactive)
@@ -192,3 +222,5 @@
        'es-aai-indent-defun) )
 
 (provide 'es-css-mode)
+
+;;; es-css-mode.el ends here
